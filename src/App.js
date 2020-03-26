36744632987da741b49f72ca111e24f5140becef
AnthSuz <mil4@hotmail.fr>
2020-03-26 11:04:29 +0100

    finish with material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import * as Actions from "./actions";
 import { makeStyles, withStyles } from "@material-ui/styles";
-import { Checkbox, TextField, Button, IconButton } from "@material-ui/core";
+import {
+  Checkbox,
+  TextField,
+  Button,
+  IconButton,
+  Typography
+} from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
@@ -30,56 +36,19 @@ const useStyles = makeStyles({
 
 const StyledButton = withStyles({
   root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     borderRadius: 0,
     borderTopRightRadius: 4,
+    borderTopLeftRadius: 4,
     border: 0,
     color: "white",
     height: 56,
     padding: "0 30px",
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
+    boxShadow: "none"
   },
   label: {
     textTransform: "capitalize"
   }
 })(Button);
-
-// position: relative;
-// transition: background-color 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms;
-// background-color: rgba(0, 0, 0, 0.09);
-// border-top-left-radius: 4px;
-// border-top-right-radius: 4px;
-
-// color: rgba(0, 0, 0, 0.54);
-//     padding: 0;
-//     font-size: 1rem;
-//     font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-//     font-weight: 400;
-//     line-height: 1;
-//     letter-spacing: 0.00938em;
-
-// const InputBase = withStyles({
-//   root: {
-//     "&after": {
-//       borderBottomColor: "green"
-//     },
-//     height: 56,
-//     position: "relative",
-//     backgroundColor: "rgba(0,0,0,0.09)",
-//     // borderTopLeftRadius: 40,
-//     // borderTopRightRadius: 4
-//     borderRadius: 0,
-//     top: 0
-//   },
-//   label: {
-//     // color: "rgba(0, 0, 0, 0.54)",
-//     padding: 0,
-//     fontSize: "1rem",
-//     fontWeight: 400,
-//     lineHeight: 1,
-//     letterSpacing: "0.00938em"
-//   }
-// })(InputBase);
 
 function App() {
   const tasks = useSelector(state => state.toDoReducers.tasks);
@@ -99,7 +68,9 @@ function App() {
             return (
               <div key={item + "_" + index} className={classes.list}>
                 <Checkbox
+                  color="primary"
                   id={item.title}
+                  checked={item.checked}
                   onChange={e =>
                     dispatch(
                       Actions.checkedTodo({
@@ -112,7 +83,7 @@ function App() {
                 />
                 {item.edit ? (
                   <>
-                    <input
+                    <TextField
                       className={classes.input}
                       value={editInputValue}
                       onChange={e =>
@@ -131,13 +102,13 @@ function App() {
                     </IconButton>
                   </>
                 ) : (
-                  //J ARRIVE PAS A MODIFIER L EDIT
-                  <label
+                  <Typography
+                    variant="h6"
                     htmlFor={item.title}
                     className={item.checked ? classes.doneTodo : ""}
                   >
                     {item.title}
-                  </label>
+                  </Typography>
                 )}
                 <IconButton
                   aria-label="delete"
@@ -164,11 +135,11 @@ function App() {
             onChange={e => dispatch(Actions.changeTodo(e.target.value))}
             label="Votre tâche"
             variant="filled"
-            className={classes.inputTodo}
           />
+
           <StyledButton
             variant="contained"
-            color="secondary"
+            color="primary"
             size="large"
             onClick={event => {
               event.preventDefault();
